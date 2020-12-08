a18ac0548770d54aaff50e5da864bdf24aaf5ba3
OdessaR <yaping.ren@hotmail.com>
2020-12-08 02:07:33 +0100

    keep only one app

--- app.py
+++ app.py
@@ -1,44 +1,67 @@
 import dash
+import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
-
 import pandas as pd
+import figure_creator as fc
+from colors import *
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+df = pd.read_csv('data/president_county_candidate.csv')
+external_stylesheets = ['/assets/style.css']
+
+external_stylesheets = [
+    dbc.themes.YETI,
+    #'https://use.fontawesome.com/releases/v5.9.0/css/all.css',
+]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        id='year-slider',
-        min=df['year'].min(),
-        max=df['year'].max(),
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
-        step=None
-    )
+
+fig_map = fc.get_map_figure()
+
+NAVBAR = dbc.NavbarSimple(
+    brand="Interactive Social Media Analysis: Twitter Sentiment to the 2020 US election",
+    #brand_href="#",
+    color="primary",
+    dark=True,
+)
+
+MAP = dbc.Card(
+    [   
+        dbc.CardHeader("2020 US election result map"),
+        dbc.CardBody(
+            [
+                dcc.Graph(
+                    id='map',
+                    figure=fig_map,
+                    config={
+                        'displayModeBar': False,
+                        #'staticPlot': True
+                    }
+                )
+            ]
+        ),
+    ], 
+    className="map_card",
+    # style={"width":"1200px"} # set style seperately
+)
+
+BODY = dbc.Container(
+    [
+        MAP
+    ],
+    className="class_name",
+)
+
+app.layout = html.Div(children=[
+    NAVBAR,
+    BODY
 ])
-
-
-@app.callback(
-    Output('graph-with-slider', 'figure'),
-    Input('year-slider', 'value'))
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-
-    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
-                     size="pop", color="continent", hover_name="country",
-                     log_x=True, size_max=55)
-
-    fig.update_layout(transition_duration=500)
-
-    return fig
 
 
 if __name__ == '__main__':
